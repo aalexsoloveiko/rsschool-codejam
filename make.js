ad76f097aa7a1f9198e4a7884945a14d25f51b85
aalexsoloveiko <43556007+aalexsoloveiko@users.noreply.github.com>
2018-10-28 19:59:48 +0300

    style

--- make.js
+++ make.js
@@ -1,9 +1,9 @@
-////Функция #1 "sumOfOther".
-//
-//function sumOfOther(arr) {
-//  let sumAll = arr.reduce((sum, current) => sum + current);
-//  return arr.map((val, i) => sumAll - val);
-//}
+//Функция #1 "sumOfOther".
+
+function sumOfOther(arr) {
+  let sumAll = arr.reduce((sum, current) => sum + current);
+  return arr.map((val, i) => sumAll - val);
+}
 
 
 //Функция #2 "make".
@@ -24,5 +24,22 @@ function make() {
   return total;
 }
 console.log(make(15)(34, 21, 666)(41));
+
+
+
+var tree = {"value":100,"left":{"value":90,"left":{"value":70},"right":{"value":99}},"right":{"value":120,"left":{"value":110},"right":{"value":130}}};
+function Test(tree){
+    var arr=[];
+    for  (var i = 0;i < arr.length;i++){
+        
+        arr.push(i);
+    };
+};
+console.log(Test);
+
+
+
+
+
 
 
