05bda6e3be0af7a0d03b3242d76c063f8f14b727
aalexsoloveiko <43556007+aalexsoloveiko@users.noreply.github.com>
2018-10-28 16:48:36 +0300

    make.js
    Second task

--- make.js
+++ make.js
@@ -1,8 +1,28 @@
-//Функция #1 "sumOfOther".
+////Функция #1 "sumOfOther".
+//
+//function sumOfOther(arr) {
+//  let sumAll = arr.reduce((sum, current) => sum + current);
+//  return arr.map((val, i) => sumAll - val);
+//}
 
-function sumOfOther(arr) {
-  let sumAll = arr.reduce((sum, current) => sum + current);
-  return arr.map((val, i) => sumAll - val);
+
+//Функция #2 "make".
+
+function make() {
+  var sum = 0;
+  for (var i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  function total() {
+    for (var i = 0; i < arguments.length; i++) {
+      sum += arguments[i];
+    }
+    return total;
+  }
+  total.toString = function () { return sum };
+
+  return total;
 }
+console.log(make(15)(34, 21, 666)(41));
 
 
